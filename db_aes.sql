7380d715a47ef3926afc36c63d4d9815b0db708c
Danico <danicobalane1120422@gmail.com>
2018-08-16 22:46:22 +0800

    docu table and area table has been added to sql databases

--- db_aes.sql
+++ db_aes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 16, 2018 at 01:19 PM
+-- Generation Time: Aug 16, 2018 at 04:45 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.0.28
 
@@ -43,8 +43,31 @@ CREATE TABLE `account_table` (
 INSERT INTO `account_table` (`id`, `username`, `password`, `full_name`, `type`) VALUES
 (1, 'admin', 'admin', 'admin_full_name', 'admin'),
 (44, 'coordinator', 'adad', 'coordinator_fullname', 'admin'),
-(48, 'www', 'www', 'www', 'admin'),
-(49, 'RG', 'admin', 'IBARRA', 'admin');
+(51, 'gfh', '345', 'jkjjhk', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `area_table`
+--
+
+CREATE TABLE `area_table` (
+  `area_id` int(11) NOT NULL,
+  `area_no` varchar(255) NOT NULL,
+  `area_desc` text NOT NULL,
+  `doc_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `document_table`
+--
+
+CREATE TABLE `document_table` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,7 +89,13 @@ CREATE TABLE `log_table` (
 INSERT INTO `log_table` (`id`, `username`, `full_name`, `description`) VALUES
 (1, 'admin', 'admin', 'unknown'),
 (2, 'admin', 'admin', 'admin added new account.'),
-(3, 'admin', 'admin', 'admin has been successfully updated an account.');
+(3, 'admin', 'admin', 'admin has been successfully updated an account.'),
+(4, 'admin', 'admin', 'admin has been successfully deleted an account.'),
+(5, 'admin', 'admin', 'admin has been successfully deleted an account.'),
+(6, 'admin', 'admin', 'admin added new account.'),
+(7, 'admin', 'admin', 'admin has been successfully deleted an account.'),
+(8, 'admin', 'admin', 'admin added new account.'),
+(9, 'admin', 'admin', 'admin has been successfully updated an account.');
 
 --
 -- Indexes for dumped tables
@@ -76,6 +105,18 @@ INSERT INTO `log_table` (`id`, `username`, `full_name`, `description`) VALUES
 -- Indexes for table `account_table`
 --
 ALTER TABLE `account_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `area_table`
+--
+ALTER TABLE `area_table`
+  ADD PRIMARY KEY (`area_id`);
+
+--
+-- Indexes for table `document_table`
+--
+ALTER TABLE `document_table`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -92,13 +133,25 @@ ALTER TABLE `log_table`
 -- AUTO_INCREMENT for table `account_table`
 --
 ALTER TABLE `account_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT for table `area_table`
+--
+ALTER TABLE `area_table`
+  MODIFY `area_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `document_table`
+--
+ALTER TABLE `document_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `log_table`
 --
 ALTER TABLE `log_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
