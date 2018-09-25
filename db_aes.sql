33572abec154aff6465021f959241b8a48a62fd1
Danico <danicobalane1120422@gmail.com>
2018-09-25 14:56:40 +0800

    update database date time logs

--- db_aes.sql
+++ db_aes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2018 at 02:58 PM
+-- Generation Time: Sep 25, 2018 at 08:55 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.0.28
 
@@ -25,67 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `account_table`
---
-
-CREATE TABLE `account_table` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `account_table`
---
-
-INSERT INTO `account_table` (`id`, `username`, `password`, `full_name`, `type`) VALUES
-(1, 'admin', 'admin', 'admin_full_name', 'admin'),
-(44, 'coordinator', 'adad', 'coordinator_fullname', 'admin'),
-(51, 'gfh', '345', 'jkjjhk', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `area_table`
---
-
-CREATE TABLE `area_table` (
-  `area_id` int(11) NOT NULL,
-  `area_no` varchar(255) NOT NULL,
-  `area_desc` text NOT NULL,
-  `doc_id` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `area_table`
---
-
-INSERT INTO `area_table` (`area_id`, `area_no`, `area_desc`, `doc_id`) VALUES
-(49, 'AREA 1', 'This are the parameters', '14');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `document_table`
---
-
-CREATE TABLE `document_table` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `document_table`
---
-
-INSERT INTO `document_table` (`id`, `title`) VALUES
-(14, 'qweqweqwe');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `log_table`
 --
 
@@ -93,45 +32,24 @@ CREATE TABLE `log_table` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `full_name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `description` varchar(255) NOT NULL,
+  `DateAndTime` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `log_table`
 --
 
-INSERT INTO `log_table` (`id`, `username`, `full_name`, `description`) VALUES
-(1, 'admin', 'admin', 'unknown'),
-(2, 'admin', 'admin', 'admin added new account.'),
-(3, 'admin', 'admin', 'admin has been successfully updated an account.'),
-(4, 'admin', 'admin', 'admin has been successfully deleted an account.'),
-(5, 'admin', 'admin', 'admin has been successfully deleted an account.'),
-(6, 'admin', 'admin', 'admin added new account.'),
-(7, 'admin', 'admin', 'admin has been successfully deleted an account.'),
-(8, 'admin', 'admin', 'admin added new account.'),
-(9, 'admin', 'admin', 'admin has been successfully updated an account.');
+INSERT INTO `log_table` (`id`, `username`, `full_name`, `description`, `DateAndTime`) VALUES
+(25, 'coordinator', 'coordinator', 'coordinator signed out. ', '9/25/2018 2:52:34 PM'),
+(26, 'admin', 'admin', 'admin logged in. ', '9/25/2018 2:52:37 PM'),
+(27, 'admin', 'admin', 'admin signed out. ', '9/25/2018 2:52:58 PM'),
+(28, 'admin', 'admin', 'admin logged in. ', '9/25/2018 2:53:03 PM'),
+(29, 'admin', 'admin', 'admin signed out. ', '9/25/2018 2:53:45 PM');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `account_table`
---
-ALTER TABLE `account_table`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `area_table`
---
-ALTER TABLE `area_table`
-  ADD PRIMARY KEY (`area_id`);
-
---
--- Indexes for table `document_table`
---
-ALTER TABLE `document_table`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `log_table`
@@ -144,28 +62,10 @@ ALTER TABLE `log_table`
 --
 
 --
--- AUTO_INCREMENT for table `account_table`
---
-ALTER TABLE `account_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
-
---
--- AUTO_INCREMENT for table `area_table`
---
-ALTER TABLE `area_table`
-  MODIFY `area_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
-
---
--- AUTO_INCREMENT for table `document_table`
---
-ALTER TABLE `document_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
 -- AUTO_INCREMENT for table `log_table`
 --
 ALTER TABLE `log_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
